73340e8c3488ea4ac5da7918026901d3c860274d
ulapod02@icloud.com <ulapod02@icloud.com>
2021-11-05 10:53:00 +0400

    laba5

--- WindowsFormsCatamaran/FormCatamaranConfig.cs
+++ WindowsFormsCatamaran/FormCatamaranConfig.cs
@@ -7,8 +7,7 @@ namespace WindowsFormsCatamaran
     public partial class FormCatamaranConfig : Form
     {
         ITransport boat = null;
-
-        private Action<Vehicle> eventAddBoat; 
+        private Action<Vehicle> eventAddBoat;
 
         public FormCatamaranConfig()
         {
@@ -25,29 +24,29 @@ namespace WindowsFormsCatamaran
         }
 
         // Отрисовать лодку
-        private void DrawBoat() 
-        { 
-            if (boat != null) 
-            { 
-                Bitmap bmp = new Bitmap(pictureBoxBoat.Width, pictureBoxBoat.Height); 
-                Graphics gr = Graphics.FromImage(bmp); 
-                boat.SetPosition(5, 5, pictureBoxBoat.Width, pictureBoxBoat.Height); 
-                boat.DrawTransport(gr); 
-                pictureBoxBoat.Image = bmp; 
-            } 
+        private void DrawBoat()
+        {
+            if (boat != null)
+            {
+                Bitmap bmp = new Bitmap(pictureBoxBoat.Width, pictureBoxBoat.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                boat.SetPosition(5, 5, pictureBoxBoat.Width, pictureBoxBoat.Height);
+                boat.DrawTransport(gr);
+                pictureBoxBoat.Image = bmp;
+            }
         }
 
         // Добавление события
-        public void AddEvent(Action<Vehicle> ev) 
-        { 
-            if (eventAddBoat == null) 
-            { 
+        public void AddEvent(Action<Vehicle> ev)
+        {
+            if (eventAddBoat == null)
+            {
                 eventAddBoat = new Action<Vehicle>(ev);
-            } 
-            else 
-            { 
-                eventAddBoat += ev; 
-            } 
+            }
+            else
+            {
+                eventAddBoat += ev;
+            }
         }
 
         // Передаем информацию при нажатии на Label
@@ -64,27 +63,27 @@ namespace WindowsFormsCatamaran
         // Проверка получаемой информации (ее типа на соответствие требуемому) 
         private void panelBoat_DragEnter(object sender, DragEventArgs e)
         {
-            if (e.Data.GetDataPresent(DataFormats.Text)) 
-            { 
-                e.Effect = DragDropEffects.Copy; 
-            } 
-            else 
-            { 
-                e.Effect = DragDropEffects.None; 
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
             }
         }
 
         // Действия при приеме перетаскиваемой информации
         private void panelBoat_DragDrop(object sender, DragEventArgs e)
         {
-            switch (e.Data.GetData(DataFormats.Text).ToString()) 
+            switch (e.Data.GetData(DataFormats.Text).ToString())
             {
-                case "Лодка": 
-                    boat = new Boat((int)numericUpDownMaxSpeed.Value, (int)numericUpDownWeight.Value, Color.White); 
-                    break; 
+                case "Лодка":
+                    boat = new Boat((int)numericUpDownMaxSpeed.Value, (int)numericUpDownWeight.Value, Color.White);
+                    break;
                 case "Катамаран":
-                    boat = new Catamaran((int)numericUpDownMaxSpeed.Value, (int)numericUpDownWeight.Value, Color.White, Color.White, true, checkBoxLeftCorpus.Checked, checkBoxRightCorpus.Checked, checkBoxSeat.Checked); 
-                    break; 
+                    boat = new Catamaran((int)numericUpDownMaxSpeed.Value, (int)numericUpDownWeight.Value, Color.White, Color.White, true, checkBoxLeftCorpus.Checked, checkBoxRightCorpus.Checked, checkBoxSeat.Checked);
+                    break;
             }
             DrawBoat();
         }
@@ -118,16 +117,12 @@ namespace WindowsFormsCatamaran
 
         // Принимаем дополнительный цвет
         private void labelDopColor_DragDrop(object sender, DragEventArgs e)
-        { 
-           /* if (boat.GetType() != typeof(Catamaran)) 
+        {
+            if (boat.GetType() != typeof(Catamaran))
                 return;
             Color DopColor = (Color)e.Data.GetData(typeof(Color));
-            Catamaran catamaran= (Catamaran)boat;
-            catamaran.SetDopColor(DopColor);*/
-            if (boat is Catamaran)
-            {
-                (boat as Catamaran).SetDopColor((Color)e.Data.GetData(typeof(Color)));
-            }
+            Catamaran catamaran = (Catamaran)boat;
+            catamaran.SetDopColor(DopColor);
             DrawBoat();
         }
 
@@ -137,6 +132,5 @@ namespace WindowsFormsCatamaran
             eventAddBoat?.Invoke((Vehicle)boat);
             Close();
         }
-
     }
 }
